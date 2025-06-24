1a149240a3c2024da3a2da1ac919c7edb7968584
dxdx <doosupark@gmail.com>
2025-06-24 11:41:42 +0900

    구글 애즈 메타태그 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "MBTI 일본 이름 생성기",
   description: "MBTI 성향에 맞는 일본식 이름을 생성해보세요!",
+  other: {
+    "google-adsense-account": "ca-pub-8759341144415814",
+  },
 };
 
 export default function RootLayout({
